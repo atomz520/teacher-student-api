1db120ce8ca6c942995d8522c8b00776e89af7df
Shalom Quek <shalomquek@gmail.com>
2022-07-14 15:25:11 +0800

    Completed the other tasks

--- src/controllers/TeacherStudentController.js
+++ src/controllers/TeacherStudentController.js
@@ -32,7 +32,6 @@ module.exports = {
             student: req.body.students[i]
           }
         });
-        console.log(pairing)
         if(!pairing) {
           await Pairing.create({
             "teacher": req.body.teacher,
@@ -49,21 +48,44 @@ module.exports = {
     }
   },
   async commonStudents (req, res) {
-    console.log(req.query)
     try {
+      let response = {}
+      let studentsRaw = {}
       let students = []
-      for (let i = 0; i < req.query.teacher.length; i++) {
+      if(typeof(req.query.teacher) === "string"){
         const pairing = await Pairing.findAll({
           where: {
-            teacher: req.query.teacher[i]
+            teacher: req.query.teacher
           }
         });
+        let studentsOfTeacher = []
         for (let j = 0; j < pairing.length; j++) {
-          students.push(pairing[j].dataValues.student)
+          studentsOfTeacher.push(pairing[j].dataValues.student)
+        }
+        students = studentsOfTeacher
+      } else {
+        for (let i = 0; i < req.query.teacher.length; i++) {
+          const pairing = await Pairing.findAll({
+            where: {
+              teacher: req.query.teacher[i]
+            }
+          });
+          let studentsOfTeacher = []
+          for (let j = 0; j < pairing.length; j++) {
+            studentsOfTeacher.push(pairing[j].dataValues.student)
+          }
+          studentsRaw[req.query.teacher[i]] = studentsOfTeacher
+        }
+        for(const [key, value] of Object.entries(studentsRaw)) {
+          if(students.length === 0) {
+            students = value
+          }
+          let intersection = students.filter(x => value.includes(x));
+          students = intersection
         }
       }
-      console.log(students)
-      res.send(habit.toJSON())
+      response.students = students
+      res.status(200).send(JSON.stringify(response))
     } catch (err) {
       res.status(400).send({
         error: `The list of students could not be retrieved`
@@ -87,11 +109,45 @@ module.exports = {
   },
   async retrieveForNotifications (req, res) {
     try {
+      let response = {}
+      let receipients = []
+      let students = await Pairing.findAll({
+        where: {
+          teacher: req.body.teacher
+        }
+      });
+      for(let student of students) {
+        receipients.push(student.student)
+      }
+      
+      let addedStudents = req.body.notification.split(' @');
+      addedStudents.shift()
 
-      res.send(habit.toJSON())
+
+      let difference = receipients
+        .filter(x => !addedStudents.includes(x))
+        .concat(addedStudents.filter(x => !receipients.includes(x)));
+
+      for (let i = 0; i < difference.length; i++) {
+        const student = await Student.findOne({
+          where: {
+            email: difference[i]
+          }
+        });
+        if(!student){
+          difference.splice(i,1)
+        } else if (student.dataValues.suspend) {
+          difference.splice(i,1)
+          i--
+        }
+      }
+      response.receipients = difference
+
+      res.status(200).send(JSON.stringify(response))
     } catch (err) {
       res.status(400).send({
-        error: `The student has not been registered`
+        message: `The student has not been registered`,
+        error: err
       })
     }
   },
