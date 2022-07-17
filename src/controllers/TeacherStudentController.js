c28c5315e53c56f78aeaccd5141791bc1bed3b0b
Shalom Quek <shalomquek@gmail.com>
2022-07-17 23:57:26 +0800

    Added comments

--- src/controllers/TeacherStudentController.js
+++ src/controllers/TeacherStudentController.js
@@ -5,33 +5,39 @@ const {Pairing} = require('../models')
 module.exports = {
   async register (req, res) {
     try {
+      // Find if database already has given teacher
       const teacher = await Teacher.findOne({
         where: {
           email: req.body.teacher
         }
       });
+      // Add teacher if not already present in database
       if (!teacher) {
         await Teacher.create({
           "email": req.body.teacher
         });
       }
       for (let i = 0; i < req.body.students.length; i++) {
+        // Find if database already has given student
         const student = await Student.findOne({
           where: {
             email: req.body.students[i]
           }
         });
+        // Add student if not already present in database
         if(!student) {
           await Student.create({
             "email": req.body.students[i]
           });
         }
+        // Find if teacher-student pairing already exists in the database
         const pairing = await Pairing.findOne({
           where: {
             teacher: req.body.teacher,
             student: req.body.students[i]
           }
         });
+        // Create pairing
         if(!pairing) {
           await Pairing.create({
             "teacher": req.body.teacher,
@@ -53,6 +59,7 @@ module.exports = {
       let studentsRaw = {}
       let students = []
       if(typeof(req.query.teacher) === "string"){
+        // Get students for single teacher provided in GET 
         const pairing = await Pairing.findAll({
           where: {
             teacher: req.query.teacher
@@ -64,6 +71,7 @@ module.exports = {
         }
         students = studentsOfTeacher
       } else {
+        // Get students for multiple teachers provided in GET 
         for (let i = 0; i < req.query.teacher.length; i++) {
           const pairing = await Pairing.findAll({
             where: {
@@ -76,6 +84,7 @@ module.exports = {
           }
           studentsRaw[req.query.teacher[i]] = studentsOfTeacher
         }
+        // Check for intersected (common) students from both arrays
         for(const [key, value] of Object.entries(studentsRaw)) {
           if(students.length === 0) {
             students = value
@@ -94,6 +103,7 @@ module.exports = {
   },
   async suspend (req, res) {
     try {
+      // Set student's property of suspend to 1 with identifier being email
       await Student.update({
           suspend: 1
         },{ where: {
@@ -111,6 +121,7 @@ module.exports = {
     try {
       let response = {}
       let receipients = []
+      // Look for student list of given teacher
       let students = await Pairing.findAll({
         where: {
           teacher: req.body.teacher
@@ -119,15 +130,14 @@ module.exports = {
       for(let student of students) {
         receipients.push(student.student)
       }
-      
+      // Students from POST JSON
       let addedStudents = req.body.notification.split(' @');
       addedStudents.shift()
-
-
+      // Union students list gotten from DB and students from JSON
       let difference = receipients
         .filter(x => !addedStudents.includes(x))
         .concat(addedStudents.filter(x => !receipients.includes(x)));
-
+      // Check if any of the students are suspended
       for (let i = 0; i < difference.length; i++) {
         const student = await Student.findOne({
           where: {
@@ -141,8 +151,8 @@ module.exports = {
           i--
         }
       }
+      // Send response back
       response.receipients = difference
-
       res.status(200).send(JSON.stringify(response))
     } catch (err) {
       res.status(400).send({
